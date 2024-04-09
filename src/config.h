194c15fae92c29f43f87e62b0f83cd344d878a7e
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-04-09 19:43:15 +0200

    windows comp

--- src/config.h
+++ src/config.h
@@ -3,7 +3,12 @@
 // SDL
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#if (WIN32)
+	#include <SDL_image.h>
+	// #include <SDL_mixer.h>
+#else
+	#include <SDL2/SDL_image.h>
+#endif // (WIN32)
 
 // BASIC
 #include <iostream>
--- src/source/render/objects.hpp
+++ src/source/render/objects.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+namespace rnd {
+	class objects_renderer {
+
+	};
+
+	class point {
+
+	};
+
+	class object {
+
+	};
+}
