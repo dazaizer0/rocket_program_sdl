3c18ebfb7880115ca1b5b2c57e7e2dde10450088
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-04-10 15:32:44 +0200

    mixer

--- src/config.h
+++ src/config.h
@@ -5,9 +5,12 @@
 #include <SDL2/SDL.h>
 #if (WIN32)
 	#include <SDL_image.h>
+    #include <SDL_mixer.h>
 	// #include <SDL_mixer.h>
 #else
 	#include <SDL2/SDL_image.h>
+    #include <SDL2/SDL_mixer.h>
+
 #endif // (WIN32)
 
 // BASIC
