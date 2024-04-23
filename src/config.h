fd2668dfdacad7853398f12397304b61cd0b4229
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-04-23 21:33:48 +0200

    new

--- src/config.h
+++ src/config.h
@@ -1,22 +1,17 @@
 #pragma once
 
-// SDL
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
+
 #if (WIN32)
 	#include <SDL_image.h>
-    #include <SDL_mixer.h>
-	// #include <SDL_mixer.h>
+	#include <SDL_mixer.h>
 #else
 	#include <SDL2/SDL_image.h>
-    #include <SDL2/SDL_mixer.h>
+	#include <SDL2/SDL_mixer.h>
 
-#endif // (WIN32)
+#endif
 
-// BASIC
 #include <iostream>
 #include <cmath>
 #include <vector>
-
-// SOURCE
-#include "math/mathy.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include "config.h"
 
-const int SCREEN_WIDTH = 650;
-const int SCREEN_HEIGHT = 500;
+
+const unsigned int SCREEN_WIDTH = 650;
+const unsigned int SCREEN_HEIGHT = 500;
 
 int main(int argc, char* args[]) {
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("yumesdl", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("yumesdl2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
 
     bool quit = false;
