d2e4a4c7070210cc15342af919459c474c389d1f
dazai <60112867+dazaizer0@users.noreply.github.com>
2024-08-17 23:08:02 +0200

    linux

--- program/main.cpp
+++ program/main.cpp
@@ -3,8 +3,19 @@
 #endif
 
 #include <SDL2/SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
+
+#if (WIN32)
+	#include <SDL_image.h>
+	#include <SDL_mixer.h>
+#else
+	#include <SDL2/SDL_image.h>
+	#include <SDL2/SDL_mixer.h>
+#endif
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
 #include "packages/core/scene.hpp"
@@ -13,6 +24,8 @@ int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("YUME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    glm::ivec2 mouse_pos;
 
     SDL_Event e;
     bool quit = false;
@@ -27,13 +40,12 @@ int main(int argc, char* argv[]) {
                 quit = true;
             }
 
-            int x, y;
-            Uint32 buttons = SDL_GetMouseState(&x, &y);
+            Uint32 buttons = SDL_GetMouseState(&mouse_pos.x, &mouse_pos.y);
             if (buttons & SDL_BUTTON(SDL_BUTTON_LEFT)) {
-                std::cout << "Left mouse button is pressed at position: " << x << ", " << y << '\n';
+                std::cout << "Left mouse button is pressed at position: " << mouse_pos.x << ", " << mouse_pos.y << '\n';
             }
             else if (buttons & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
-                std::cout << "Right mouse button is pressed at position: " << x << ", " << y << '\n';
+                std::cout << "Right mouse button is pressed at position: " << mouse_pos.x << ", " << mouse_pos.y << '\n';
             }
         }
 
